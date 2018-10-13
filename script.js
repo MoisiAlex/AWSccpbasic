8ee4a7845248cb4c52f6ad9bb16bc6b1a6ad8edd
Alex M <dracken@gmail.com>
2018-10-12 17:53:13 -0700

    hit api

--- script.js
+++ script.js
@@ -71,9 +71,13 @@
           var data = JSON.parse(this.response);
 
           if (request.status >= 200 && request.status < 400) {
-             console.log("recieved data: "+data);
+             console.log(data);
               document.getElementById('calls').innerHTML = data.CONTACTS_IN_QUEUE;
-              document.getElementById('lwt').innerHTML = data.OLDEST_CONTACT_AGE;
+              
+              let lwt = data.OLDEST_CONTACT_AGE;
+              let oldest = 0
+              (lwt> 60) ? oldest = parseInt(lwt)/60 +" min" : oldest = lwt + " sec";
+              document.getElementById('lwt').innerHTML = oldest;
               document.getElementById('availableAgents').innerHTML = data.AGENTS_AVAILABLE;
               document.getElementById('onlineAgents').innerHTML = data.AGENTS_ONLINE;
              
